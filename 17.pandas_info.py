854c301abe5b15bbeffdb8176e234331821593c8
Declan Andrew <anysweetimes@naver.com>
2021-01-18 00:26:53 +0900

    iloc, loc 추가

---
+++ 17.pandas_info.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+df = pd.read_csv('./data/korean-idol.csv')
+print(df.head())
+
+# column을 선택하는 방법 
+print(df['이름'], df.그룹, df["소속사"])
+
+
+# 범위 선택(range selection)
+# 단순 index에 대한 범위 선택 (column이 아닌 행 기준)
+#print(df['이름'][:3]) df[:3]
+
+
+# loc 굉장히 많이 사용 (왼쪽은 행, 오른쪽은 열)
+# print(df.loc[:, '이름'])
+
+# 부분 선택
+# print(df.loc[:, ["이름", "생년월일"]])
+
+# print(df.loc[3:8, ["이름", "생년월일"]])
+ 
+# 행은 2부터 5까지, 이름부터 생년월일 column 까지 (생년월일 전까지가 아닌 포함) Numpy와 다름
+# print(df.loc[2:5, "이름":"생년월일"])
+
+#iloc(position으로 색인)
+# print(df.iloc[:, [0, 2]])
+# iloc는 numpy와 동일하게 동작
+print(df.iloc[1:5, [0, 2]])
